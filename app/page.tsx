ea1ceaac2228d859a3055481cbb1dda60c48ad2e
CojaDev <amarcetic04@gmail.com>
2023-11-15 08:42:06 +0100

    fix light switch

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,11 @@ import Hero from '@/components/Hero';
 import Kategorije from '@/components/Kategorije';
 import { RERSTORANI } from '@/constants/restorani';
 
-import { useState, useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
 import CardSlider from '@/components/CardSlider';
 import DiscoverSection from '@/components/DiscoverySection';
 import CTASection from '@/components/CTASection';
 
 export default function Home() {
-  //lenis Smooth Scroll
-  const lenis = new Lenis();
-  useEffect(() => {
-    // Check if the device is not a mobile device
-    if (!/Mobi|Android/i.test(navigator.userAgent)) {
-      lenis.on('scroll', (e: any) => {});
-
-      const raf = function (time: any) {
-        lenis.raf(time);
-
-        requestAnimationFrame(raf);
-      };
-
-      requestAnimationFrame(raf);
-    }
-  }, [lenis]);
-
   const Najnovije = RERSTORANI.slice(RERSTORANI.length - 8, RERSTORANI.length);
   const Popularno = RERSTORANI.slice(0, 8);
   return (
