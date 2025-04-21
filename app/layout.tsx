5432e9271968f8ec23626f7f0204839384bf79c2
Aleksandar Marcetic <68874819+CojaDev@users.noreply.github.com>
2025-04-21 19:18:59 +0200

    Update layout.tsx
    adding google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><meta name="google-site-verification" content="PcVXkY3TbcO5DZLopYqYf5uz2Jgk8bEt2-W6E1Fzupw" /></head>
       <body className={inter.className}>
         <div className="main after:dark:invert-0 before:main_bg_white after:dark:opacity-20  bg-white dark:bg-gray-900 sm:dark:bg-gray-950 dark:before:main_bg_black">
           <div className="gradient " />
